08385e677529d17f2cc1a58dbdacbdd71d6cd465
서한나 <hanna@seohannaui-MacBookAir.local>
2024-07-27 12:30:43 +0900

    feat/css-home

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -15,7 +15,7 @@ const gradient = [
 const background = ["#0c3aa5", "#f1ca0b", "#039754", "#ab181b"];
 const title = ["Books", "Members", "WISHLIST", "Fun"];
 
-const TopLeft = ({ index }) => {
+const TopLeft = ({ index, ClickLogIn, ClickSignUp }) => {
   return (
     <div className={cx("top-left")}>
       <Link to="/" className={cx("left-title")}>
@@ -25,12 +25,14 @@ const TopLeft = ({ index }) => {
         <div
           className={cx("btn")}
           style={{ backgroundColor: background[index] }}
+          onClick={ClickLogIn}
         >
           LOG IN
         </div>
         <div
           className={cx("btn")}
           style={{ backgroundColor: background[index] }}
+          onClick={ClickSignUp}
         >
           SIGN UP
         </div>
@@ -39,13 +41,87 @@ const TopLeft = ({ index }) => {
   );
 };
 
-const LogIn = () => {
+const LogIn = ({ logIn, CancelLogIn }) => {
+  return (
+    <div className={cx("form", { visible: logIn })}>
+      <div className={cx("nav")}>
+        <div className={cx("cancel")} onClick={CancelLogIn}>
+          <div className={cx("left")}></div>
+          <div className={cx("right")}></div>
+        </div>
+      </div>
+      <div className={cx("center")}>
+        <div className={cx("form-title")}>LOG IN</div>
+        <form method="post" action="/auth/login">
+          <input
+            className={cx("input", "common")}
+            name="email"
+            type="email"
+            placeholder="e-mail*"
+          />
+          <input
+            className={cx("input", "common")}
+            name="password"
+            type="password"
+            placeholder="password*"
+          />
+          <input
+            className={cx("input", "common", "button")}
+            type="submit"
+            value="LOG IN"
+          />
+        </form>
+        <Link to="/auth/kakao" className={cx("kakao", "common")}>
+          KAKAO
+        </Link>
+        <Link to="/auth/naver" className={cx("naver", "common")}>
+          NAVER
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-}
-
-const SignUp = () => {
-
-}
+const SignUp = ({ signUp, CancelSignUp }) => {
+  return (
+    <div className={cx("form", { visible: signUp })}>
+      <div className={cx("nav")}>
+        <div className={cx("cancel")} onClick={CancelSignUp}>
+          <div className={cx("left")}></div>
+          <div className={cx("right")}></div>
+        </div>
+      </div>
+      <div className={cx("center")}>
+        <div className={cx("form-title")}>SIGN UP</div>
+        <form method="post" action="/auth/signup">
+          <input
+            className={cx("input", "common")}
+            type="email"
+            name="email"
+            placeholder="e-mail*"
+          />
+          <input
+            className={cx("input", "common")}
+            type="text"
+            name="nick"
+            placeholder="nickname*"
+          />
+          <input
+            className={cx("input", "common")}
+            type="password"
+            name="password"
+            placeholder="password*"
+          />
+          <input
+            className={cx("input", "common", "button")}
+            type="submit"
+            value="SIGN UP"
+          />
+        </form>
+      </div>
+    </div>
+  );
+};
 
 const BotLeft = ({ index }) => {
   return (
@@ -71,8 +147,9 @@ const TopRight = ({ index }) => {
   const Right = useRef(null);
   const Scroll = useRef(null);
   useEffect(() => {
-    const distance = Right.current.offsetHeight - Scroll.current.offsetHeight - 10;
-    document.documentElement.style.setProperty('--distance', distance + 'px');
+    const distance =
+      Right.current.offsetHeight - Scroll.current.offsetHeight - 10;
+    document.documentElement.style.setProperty("--distance", distance + "px");
   }, []);
   return (
     <div
@@ -81,11 +158,17 @@ const TopRight = ({ index }) => {
       ref={Right}
     >
       <div className={cx("right-titles")}>
-        <Link to={`/${title[index].toLowerCase()}`} className={cx("right-title")}>
+        <Link
+          to={`/${title[index].toLowerCase()}`}
+          className={cx("right-title")}
+        >
           {title[index]}
         </Link>
-        <Link to={`/${title[index].toLowerCase()}`} className={cx("right-title", "italic")}>
-        {title[index]}
+        <Link
+          to={`/${title[index].toLowerCase()}`}
+          className={cx("right-title", "italic")}
+        >
+          {title[index]}
         </Link>
       </div>
       <div className={cx("top-right-bot")}>
@@ -98,7 +181,9 @@ const TopRight = ({ index }) => {
             <div>/</div>
             <div>4</div>
           </div>
-          <div className={cx("scroll")} ref={Scroll}>Scroll</div>
+          <div className={cx("scroll")} ref={Scroll}>
+            Scroll
+          </div>
         </div>
       </div>
     </div>
@@ -112,6 +197,24 @@ const Home = () => {
   });
   const [step, setStep] = useState(null);
   const [index, setIndex] = useState(0);
+  const [logIn, setLogin] = useState(false);
+  const [signUp, setSignUp] = useState(false);
+
+  const ClickLogIn = useCallback(() => {
+    setLogin(true);
+  }, []);
+
+  const ClickSignUp = useCallback(() => {
+    setSignUp(true);
+  }, []);
+
+  const CancelLogIn = useCallback(() => {
+    setLogin(false);
+  }, []);
+
+  const CancelSignUp = useCallback(() => {
+    setSignUp(false);
+  }, []);
 
   const scrollHandler = (e) => {
     if (window.pageYOffset >= range.start && window.pageYOffset < range.end) {
@@ -143,7 +246,13 @@ const Home = () => {
     <div className={cx("home")}>
       <div className={cx("sticky")}>
         <div className={cx("top")}>
-          <TopLeft index={index} />
+          <TopLeft
+            index={index}
+            ClickLogIn={ClickLogIn}
+            ClickSignUp={ClickSignUp}
+          />
+          <LogIn logIn={logIn} CancelLogIn={CancelLogIn} />
+          <SignUp signUp={signUp} CancelSignUp={CancelSignUp} />
           <TopRight index={index} />
         </div>
         <div className={cx("bot")}>
