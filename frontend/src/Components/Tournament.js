e8af1267c3f056011c8a932bdc0e95832a9de3f7
서한나 <hanna@seohannaui-MacBookAir.local>
2024-07-29 14:47:52 +0900

    feat/tournament

--- frontend/src/Components/Tournament.js
+++ frontend/src/Components/Tournament.js
@@ -3,12 +3,12 @@ import React, {
   useEffect,
   useReducer,
   useRef,
-  useState,
 } from "react";
 import styles from "../Css/Tournament.module.css";
 import classNames from "classnames/bind";
 import { Link } from "react-router-dom";
 import { produce } from "immer";
+import { tournamentState, reducer } from '../Modules/Tournament';
 
 const cx = classNames.bind(styles);
 
@@ -294,134 +294,6 @@ const BotInfo = ({ state, dispatch }) => {
   );
 };
 
-const tournamentState = {
-  index: 0,
-  final: false,
-  random: [],
-  original: [],
-  info: {},
-  main: [],
-  sub: [],
-  tem: {
-    main: [],
-    sub: [],
-    random: [],
-  },
-  current: 0,
-  total: 0,
-  play: {
-    top: false,
-    bottom: false,
-  },
-  choosen: null,
-  finalClicked: false,
-};
-
-function reducer(state, action) {
-  switch (action.type) {
-    case "INIT":
-      return produce(state, (draft) => {
-        draft.original = JSON.parse(action.data.original);
-        draft.info = {
-          id: action.data.id,
-          title: action.data.title,
-          model: action.data.modelName,
-          types: action.data.types,
-          exp: action.data.explanation,
-        };
-        draft.random = makeRandom(action.round);
-        draft.current = 1;
-        draft.total = +action.round / 2;
-      });
-    case "SELECTED":
-      return produce(state, (draft) => {
-        draft.original.forEach((item) => {
-          item.selected++;
-        });
-      });
-    case "MAINSUB":
-      return produce(state, (draft) => {
-        draft.main = state.random.map((item) => {
-          return state.original[item].main;
-        });
-        draft.sub = state.random.map((item) => {
-          return state.original[item].sub;
-        });
-      });
-    case "TOP_PLAY":
-      return produce(state, (draft) => {
-        draft.play.top = true;
-        draft.play.bottom = false;
-      });
-    case "TOP_PAUSE":
-      return produce(state, (draft) => {
-        draft.play.top = false;
-      });
-    case "BOTTOM_PLAY":
-      return produce(state, (draft) => {
-        draft.play.top = false;
-        draft.play.bottom = true;
-      });
-    case "BOTTOM_PAUSE":
-      return produce(state, (draft) => {
-        draft.play.bottom = false;
-      });
-    case "CLICKED":
-      const choosen =
-        action.position === "top"
-          ? state.random[state.index]
-          : state.random[state.index + 1];
-      return produce(state, (draft) => {
-        draft.choosen = choosen;
-        draft.original[choosen].win++;
-        draft.original[choosen].selected++;
-        draft.tem.random[draft.tem.random.length] = choosen;
-        draft.tem.main[draft.tem.main.length] = draft.original[choosen].main;
-        draft.tem.sub[draft.tem.sub.length] = draft.original[choosen].sub;
-        draft.index += 2;
-        draft.play.top = false;
-        draft.play.bottom = false;
-        draft.current++;
-      });
-    case "NEXT_ROUND":
-      return produce(state, (draft) => {
-        draft.current = 1;
-        draft.total /= 2;
-        draft.index = 0;
-        draft.main = draft.tem.main;
-        draft.sub = draft.tem.sub;
-        draft.random = draft.tem.random;
-        draft.tem.main = [];
-        draft.tem.sub = [];
-        draft.tem.random = [];
-      });
-    case "GO_FINAL":
-      return produce(state, (draft) => {
-        draft.final = true;
-        draft.index = 0;
-        draft.main = draft.tem.main;
-        draft.sub = draft.tem.sub;
-        draft.random = draft.tem.random;
-        draft.tem.main = [];
-        draft.tem.sub = [];
-        draft.tem.random = [];
-      });
-    case "FINAL_CLICKED":
-      const finalNumber =
-        action.position === "top"
-          ? state.random[state.index]
-          : state.random[state.index + 1];
-      return produce(state, (draft) => {
-        draft.choosen = finalNumber;
-        draft.original[finalNumber].win++;
-        draft.original[finalNumber].finalWin++;
-        draft.play.top = false;
-        draft.play.bottom = false;
-        draft.finalClicked = true;
-      });
-  }
-}
-
 const Battle = ({ state, dispatch }) => {
   useEffect(() => {
     if (state.current > state.total && state.total !== 2) {
@@ -441,15 +313,6 @@ const Battle = ({ state, dispatch }) => {
       <BotInfo state={state} dispatch={dispatch} />
     </div>
   );
-};
-
-const makeRandom = (round) => {
-  const arr = Array.from({ length: 32 }, (_, i) => i); // 0부터 31까지 숫자 배열 생성
-  for (let i = arr.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [arr[i], arr[j]] = [arr[j], arr[i]]; // 요소를 무작위로 섞음
-  }
-  return arr.slice(0, round); // 필요한 개수만큼 잘라서 반환
 };
 
 const Tournament = ({ category, loading, id, round }) => {
--- frontend/src/Modules/Tournament.js
+++ frontend/src/Modules/Tournament.js
@@ -1,11 +1,12 @@
 import { handleActions } from "redux-actions";
 import axios from "axios";
+import { produce } from "immer";
 
 const GET_CATEGORY = "Category/GET_CATEGORY";
 const GET_CATEGORY_SUCCESS = "Category/GET_CATEGORY_SUCCESS";
 const GET_CATEGORY_FAILURE = "Category/GET_CATEGORY_FAILURE";
 
-export const getCategory = (id, round) => async (dispatch) => {
+const getCategory = (id, round) => async (dispatch) => {
   dispatch({ type: GET_CATEGORY });
   try {
     const response = await axios.get(`/favorite/${id}/${round}`);
@@ -45,3 +46,143 @@ const tournament = handleActions(
 );
 
 export default tournament;
+
+const tournamentState = {
+  index: 0,
+  final: false,
+  random: [],
+  original: [],
+  info: {},
+  main: [],
+  sub: [],
+  tem: {
+    main: [],
+    sub: [],
+    random: [],
+  },
+  current: 0,
+  total: 0,
+  play: {
+    top: false,
+    bottom: false,
+  },
+  choosen: null,
+  finalClicked: false,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "INIT":
+      return produce(state, (draft) => {
+        draft.original = JSON.parse(action.data.original);
+        draft.info = {
+          id: action.data.id,
+          title: action.data.title,
+          model: action.data.modelName,
+          types: action.data.types,
+          exp: action.data.explanation,
+        };
+        draft.random = makeRandom(action.round);
+        draft.current = 1;
+        draft.total = +action.round / 2;
+      });
+    case "SELECTED":
+      return produce(state, (draft) => {
+        draft.original.forEach((item) => {
+          item.selected++;
+        });
+      });
+    case "MAINSUB":
+      return produce(state, (draft) => {
+        draft.main = state.random.map((item) => {
+          return state.original[item].main;
+        });
+        draft.sub = state.random.map((item) => {
+          return state.original[item].sub;
+        });
+      });
+    case "TOP_PLAY":
+      return produce(state, (draft) => {
+        draft.play.top = true;
+        draft.play.bottom = false;
+      });
+    case "TOP_PAUSE":
+      return produce(state, (draft) => {
+        draft.play.top = false;
+      });
+    case "BOTTOM_PLAY":
+      return produce(state, (draft) => {
+        draft.play.top = false;
+        draft.play.bottom = true;
+      });
+    case "BOTTOM_PAUSE":
+      return produce(state, (draft) => {
+        draft.play.bottom = false;
+      });
+    case "CLICKED":
+      const choosen =
+        action.position === "top"
+          ? state.random[state.index]
+          : state.random[state.index + 1];
+      return produce(state, (draft) => {
+        draft.choosen = choosen;
+        draft.original[choosen].win++;
+        draft.original[choosen].selected++;
+        draft.tem.random[draft.tem.random.length] = choosen;
+        draft.tem.main[draft.tem.main.length] = draft.original[choosen].main;
+        draft.tem.sub[draft.tem.sub.length] = draft.original[choosen].sub;
+        draft.index += 2;
+        draft.play.top = false;
+        draft.play.bottom = false;
+        draft.current++;
+      });
+    case "NEXT_ROUND":
+      return produce(state, (draft) => {
+        draft.current = 1;
+        draft.total /= 2;
+        draft.index = 0;
+        draft.main = draft.tem.main;
+        draft.sub = draft.tem.sub;
+        draft.random = draft.tem.random;
+        draft.tem.main = [];
+        draft.tem.sub = [];
+        draft.tem.random = [];
+      });
+    case "GO_FINAL":
+      return produce(state, (draft) => {
+        draft.final = true;
+        draft.index = 0;
+        draft.main = draft.tem.main;
+        draft.sub = draft.tem.sub;
+        draft.random = draft.tem.random;
+        draft.tem.main = [];
+        draft.tem.sub = [];
+        draft.tem.random = [];
+      });
+    case "FINAL_CLICKED":
+      const finalNumber =
+        action.position === "top"
+          ? state.random[state.index]
+          : state.random[state.index + 1];
+      return produce(state, (draft) => {
+        draft.choosen = finalNumber;
+        draft.original[finalNumber].win++;
+        draft.original[finalNumber].finalWin++;
+        draft.play.top = false;
+        draft.play.bottom = false;
+        draft.finalClicked = true;
+      });
+  }
+}
+
+const makeRandom = (round) => {
+  const arr = Array.from({ length: 32 }, (_, i) => i); // 0부터 31까지 숫자 배열 생성
+  for (let i = arr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]]; // 요소를 무작위로 섞음
+  }
+  return arr.slice(0, round); // 필요한 개수만큼 잘라서 반환
+};
+
+
+export { getCategory, tournamentState, reducer };
